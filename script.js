8c2ffb30b7be6c5859abb529575f85a4d12c4f26
Akshay Laxmipathi <52963912+Akshay-Laxmipathi@users.noreply.github.com>
2024-09-13 19:19:19 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
 function stringChop(str, size) {
   // your code here
+	if(!str) {
+		return [];
+	}
+	let chunks = [];
+	for(let i=0; i<str.size; i++) {
+		let subStr = slice(i, i+slice);
+		chunks.push(substr);
+	}
+	return chunks;
 }
 
 // Do not change the code below
